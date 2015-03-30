ff7d67ce0849287067a5e4edce5a2969f8ad19dc
Bin- <denis.genalitsky@gmail.com>
2015-03-30 15:03:25 +0600

    финально имплементед, оптимизатион рикваред

--- Ordered.h
+++ Ordered.h
@@ -23,29 +23,14 @@ public:
 		}
 	};
 
-	struct Hasher {
-		size_t operator()(const Key &it) const {
-			std::hash<unsigned long> hash_fn;
-			return hash_fn(it.first);
-		}
-	};
-
 	struct PtrEqual {
 		size_t operator()(const Key* lhs, const Key* rhs) const {
 			return lhs->first == lhs->first;
 		}
 	};
 
-	struct Equal
-	{
-		bool operator()(const Key &lhs, const Key &rhs) {
-			return lhs.first == rhs.first;
-		}
-	};
-
-
-	std::vector<std::unordered_set<Key, Hasher, Equal>> data;
-	std::unordered_set<Key, Hasher, Equal> hashSet;
+	std::vector<std::unordered_set<Key*, PtrHasher, PtrEqual>> data;
+	std::unordered_set<Key*, PtrHasher, PtrEqual> hashSet;
 	size_t wLen;
 	int exp;
 
@@ -54,41 +39,33 @@ public:
 	}
 
 	void init() {
-		data.push_back(std::unordered_set<Key, Hasher, Equal>());
+		data.push_back(std::unordered_set<Key*, PtrHasher, PtrEqual>());
 		for (int i = 0; i < pow(2, exp); i++) {
-			//auto pair = new std::pair<unsigned long, int>(rand() % 1000, 0);
-			auto pair = Key(i, 0);
+			auto pair = new Key(i, 0);
 			hashSet.insert(pair);
-
 			data[0].insert(pair);
-			//auto delta = pair->second - data.size() - 1;
-			//if (delta > 0) {
-			//	for (int i = 0; i < delta; i++) {
-			//		data.push_back(std::unordered_set<Key>());
-			//	}
-			//}
-			
 		}
-		std::cout << "Done!" << std::endl;
 	}
 
 	void find(Key key) {
-		auto found = hashSet.find(key);
-
+		auto found = hashSet.find(&key);
 		if (found != hashSet.end()) {
-			//std::cout << "getting element from vector" << std::endl;
-			auto freq = (*found).second;
+			auto freq = (*found)->second;
 			if (freq == data.size() - 1) {
-				data.push_back(std::unordered_set<Key, Hasher, Equal>());
+				data.push_back(std::unordered_set<Key*, PtrHasher, PtrEqual>());
 			}
-			auto sec = (*found).second;
-			auto new_pair = Key((*found).first, sec);
-			data[(*found).second].erase(found);
-			data[new_pair.second].insert(new_pair);
+			auto sec = (*found)->second;
+			auto new_pair = new Key((*found)->first, sec);
+			data[sec].erase(*found);
+			hashSet.erase(*found);
+			data[++(*new_pair).second].insert(new_pair);
+			hashSet.insert(new_pair);
 		}
 
 		else {
-			std::cout << "cyka" << std::endl;
+			key.second = 1;
+			data[1].insert(&key);
+			data[0].erase(--std::end(data[0]));
 		}
 	}
 
@@ -96,7 +73,7 @@ public:
 		for (int i = 0; i < data.size(); i++) {
 			std::cout << "\t"<< "freq " << i << std::endl;
 			for (auto it = std::begin(data[i]); it != std::end(data[i]); ++it) {
-				std::cout << (*it).first << "\t" << (*it).second << std::endl;
+				std::cout << (*it)->first << "\t" << (*it)->second << std::endl;
 			}
 			std::cout << "-------------------" << std::endl;
 		}
